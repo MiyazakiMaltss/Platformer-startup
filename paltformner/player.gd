61f0534796686c43b7f2391b4af5d35c37b1227c
Miyazaki <iansalvador127@gmail.com>
2026-09-21 00:14:08 +0800

    made a level

--- paltformner/player.gd
+++ paltformner/player.gd
@@ -2,13 +2,14 @@ extends CharacterBody2D
 
 
 const SPEED = 900.0
-const JUMP_VELOCITY = -900.0
+const JUMP_VELOCITY = -1300.0
+const GRAVITY = 2500
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += GRAVITY * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
