8d57c9fbda8b46fc685bc7ace7e618061d954353
Miyazaki <iansalvador127@gmail.com>
2026-09-21 16:03:57 +0800

    added win screen and win zone

--- paltformner/k_zone.gd
+++ paltformner/k_zone.gd
@@ -6,7 +6,5 @@ func _on_body_entered(body):
 	print("Try Again")
 	timer.start()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_timer_timeout():
 	get_tree().reload_current_scene()
